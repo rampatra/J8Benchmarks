547c02e1cee053b4a1200836ed95b329b7cc769b
ramswaroop <ramswarooppatra@gmail.com>
2016-12-28 20:44:29 +0000

    Added comments

--- src/main/java/me/ramswaroop/LambdaVsAnonymousClass.java
+++ src/main/java/me/ramswaroop/LambdaVsAnonymousClass.java
@@ -1,34 +1,3 @@
-/*
- * Copyright (c) 2014, Oracle America, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- *  * Neither the name of Oracle nor the names of its contributors may be used
- *    to endorse or promote products derived from this software without
- *    specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package me.ramswaroop;
 
 import org.openjdk.jmh.annotations.Benchmark;
@@ -42,6 +11,18 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * The below benchmarks compares the average time for a single execution of the benchmark methods for
+ * lambda (non-capturing and capturing) and anonymous classes.
+ * <p>
+ * Some useful links:
+ * 1. <a href="http://cr.openjdk.java.net/~briangoetz/lambda/lambda-translation.html">Lambda Translation</a>
+ * 2. <a href="https://www.infoq.com/articles/Java-8-Lambdas-A-Peek-Under-the-Hood">Lambdas under the hood</a>
+ * 3. <a href="http://wiki.jvmlangsummit.com/images/7/7b/Goetz-jvmls-lambda.pdf">Brian Goetz presentation on Lambdas</a>
+ *
+ * @author ramswaroop
+ * @version 27/12/2016
+ */
 public class LambdaVsAnonymousClass {
 
     @Benchmark
